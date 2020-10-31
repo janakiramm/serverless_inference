5bf6b8e1d8d52981dac81c145b176b96ec83b0b2
janakiramm <janakiramm@gmail.com>
2020-10-31 11:34:38 +0530

    Upated script

--- ec2/config.sh
+++ ec2/config.sh
@@ -1,6 +1,7 @@
 sudo chown ec2-user:ec2-user /mnt/efs/fs1
-mkdir -p /mnt/efs/fs1/ml/lib
-mkdir -p /mnt/efs/fs1/ml/model
+sudo mkdir -p /mnt/efs/fs1/ml/lib
+sudo mkdir -p /mnt/efs/fs1/ml/model
+sudo chown -R ec2-user:ec2-user /mnt/efs/fs1
 
 sudo amazon-linux-extras install python3.8
 sudo ln -s /usr/bin/python3.8 /usr/bin/python3
@@ -8,5 +9,5 @@ curl -O https://bootstrap.pypa.io/get-pip.py
 python3 get-pip.py --user
 pip3 install  -t /mnt/efs/fs1/ml/lib -r requirements.txt
 
-cp -R models/* /mnt/efs/fs1/ml/model/
+cp -R model/* /mnt/efs/fs1/ml/model/
 sudo chown -R 1001:1001 /mnt/efs/fs1/ml/
