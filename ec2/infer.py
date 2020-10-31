4d1dac6c6874af7cccd70502c6054e95958f2f2c
janakiramm <janakiramm@gmail.com>
2020-10-31 11:56:02 +0530

    Upated script

--- ec2/infer.py
+++ ec2/infer.py
@@ -67,6 +67,6 @@ def predict():
 if __name__ == "__main__":
     print("Loading infernce server")       
     initalize()
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=80)
 
             
