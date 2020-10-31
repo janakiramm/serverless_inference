c80f000c67b63c5d2172fe86d1b3482d04813f43
janakiramm <janakiramm@gmail.com>
2020-10-31 11:46:50 +0530

    Upated script

--- ec2/config.sh
+++ ec2/config.sh
@@ -3,7 +3,7 @@ sudo mkdir -p /mnt/efs/fs1/ml/lib
 sudo mkdir -p /mnt/efs/fs1/ml/model
 sudo chown -R ec2-user:ec2-user /mnt/efs/fs1
 
-sudo amazon-linux-extras install python3.8
+sudo amazon-linux-extras install -y python3.8
 sudo ln -s /usr/bin/python3.8 /usr/bin/python3
 curl -O https://bootstrap.pypa.io/get-pip.py
 python3 get-pip.py --user
